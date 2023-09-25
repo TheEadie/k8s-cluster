61bc3d0d651b296d0ae4170b5fc0f349d443520e
David Eadie <david@eadie.net>
2023-09-25 21:36:47 +0100

    Also reconcile CRDs

--- reconcile.sh
+++ reconcile.sh
@@ -3,5 +3,6 @@ set -e pipefail
 
 flux reconcile source git flux-system
 flux reconcile kustomization flux-system
+flux reconcile kustomization crds
 flux reconcile kustomization core
 flux reconcile kustomization apps
